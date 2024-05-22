d62c48263a1fba8dd4a87aa08e234622d4dae0b7
Trần Trương Văn <vanttse170128@fpt.edu.vn>
2024-05-23 00:06:21 +0700

    save work

--- src/routes/_root-layout/index.lazy.tsx
+++ src/routes/_root-layout/index.lazy.tsx
@@ -7,7 +7,7 @@ export const Route = createLazyFileRoute('/_root-layout/')({
 function Index() {
   return (
     <div className='p-2'>
-      <h3>Welcome Home!</h3>
+      <h3 className='text-gradient font-bold'>Welcome Home!</h3>
     </div>
   )
 }
