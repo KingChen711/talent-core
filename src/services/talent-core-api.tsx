3735f77d7d846fff81aeb330a2a2641a9fc8722e
Trần Trương Văn <vanttse170128@fpt.edu.vn>
2024-06-08 00:29:15 +0700

    fix hard code api url

--- src/services/talent-core-api.tsx
+++ src/services/talent-core-api.tsx
@@ -5,7 +5,7 @@ class TalentCoreApi {
 
   constructor() {
     this.instance = axios.create({
-      baseURL: 'http://localhost:5000',
+      baseURL: import.meta.env.VITE_TALENT_CORE_API_URL,
       timeout: 10000,
       headers: {
         'Content-Type': 'application/json'
